c0b17411f47c8763dba22e169d9fdf524d10c662
Roger <rogerpuigdemasa@gmail.com>
2023-11-29 10:59:54 +0100

    update firebase

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ import {
 } from "react-router-dom";
 import App from './App';
 import Gracias from './pages/Gracias';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6nH9SO7srB2LhSZaKebU1_aaDmD52ZNQ",
+  authDomain: "mobilfront-e2dc3.firebaseapp.com",
+  projectId: "mobilfront-e2dc3",
+  storageBucket: "mobilfront-e2dc3.appspot.com",
+  messagingSenderId: "481538460651",
+  appId: "1:481538460651:web:cbda7c09bb612dd2b227ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 const router = createBrowserRouter([
   {
     path: "/register/gracias",
